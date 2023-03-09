963ad76c16c111922683d047011fe97a2957cfc1
Damien Maguire <listerlt1@gmail.com>
2023-03-09 10:54:59 +0000

    update gate sd fix

--- src/stm32_sine.cpp
+++ src/stm32_sine.cpp
@@ -100,6 +100,7 @@ static void Ms100Task(void)
       can->SendAll();
 
    //TeslaSpi::TlfErrChk();
+   /*
   if(GateDriver::IsFaulty())
   {
     Param::SetInt(Param::GTStat,0);
@@ -108,6 +109,7 @@ static void Ms100Task(void)
   {
     Param::SetInt(Param::GTStat,1);
   }
+  */
 }
 
 static void RunCharger(float udc)
@@ -235,14 +237,14 @@ static void Ms10Task(void)
       //this applies new deadtime and pwmfrq and enables the outputs for the given mode
       PwmGeneration::SetOpmode(opmode);
       DigIo::err_out.Clear();
-      DigIo::Gate_SD.Clear();//enable gate drivers
+      DigIo::Gate_SD.Set();//enable gate drivers
       DigIo::prec_out.Clear();
       initWait = -1;
    }
    else if (initWait == 10)
    {
       PwmGeneration::SetCurrentOffset(AnaIn::il1.Get(), AnaIn::il2.Get());
-      DigIo::Gate_SD.Set();//disable gate drivers
+     // DigIo::Gate_SD.Clear();//disable gate drivers
       initWait--;
    }
    else if (initWait > 0)
@@ -410,7 +412,7 @@ extern "C" int main(void)
         //printf("Failed with %d\n", status);
         Param::SetInt(Param::TLFStat,0);
     }
-
+/*
     if (GateDriver::Init())
     {
         //printf("Successful\n");
@@ -422,7 +424,7 @@ extern "C" int main(void)
         //printf("Failed\n");
         Param::SetInt(Param::GTStat,0);
     }
-
+*/
 
    Stm32Scheduler s(hwRev == HW_BLUEPILL ? TIM4 : TIM2); //We never exit main so it's ok to put it on stack
    scheduler = &s;
@@ -448,6 +450,7 @@ extern "C" int main(void)
    write_bootloader_pininit(Param::GetBool(Param::bootprec), Param::GetBool(Param::pwmpol));
    DigIo::OE_245.Set();
    //TeslaSpi::InitPmic();
+   GateDriver::Enable();
 
    while(1)
       t.Run();
